e2383135986043e047a643d6192dbe7f4d27f9bc
Josef Tschiggerl <josef@tschiggerl.name>
2020-09-25 10:24:55 +0200

    restructure

--- coviddata.py
+++ coviddata.py
@@ -1,24 +1,24 @@
 import requests, csv, configparser
 import paho.mqtt.client as mqtt
 
-config = configparser.ConfigParser()
-config.sections()
-config.read('coviddata.ini')
-
-url = config['opendata']['csvurl']
-bezirke = config['covid']['bezirke']
-
 def on_connect(client, userdata, flags, rc):
     if rc != 0:
         print("Connected not successfull - error code is " + str(rc))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-
-client.connect(config['mqtt']['mqtthost'], int(config['mqtt']['mqttport']), int(config['mqtt']['mqttkeepalive']))
-client.loop_start()
-
 def main():
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read('coviddata.ini')
+
+    url = config['opendata']['csvurl']
+    bezirke = config['covid']['bezirke']
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+
+    client.connect(config['mqtt']['mqtthost'], int(config['mqtt']['mqttport']), int(config['mqtt']['mqttkeepalive']))
+    client.loop_start()
+
     with requests.Session() as s:
         
         try:
@@ -35,6 +35,5 @@ def main():
                 client.publish("health/covid/anzahl/"+str(row["Bezirk"]), row["Anzahl"])
                 print(row["Bezirk"],":",row["Anzahl"],":",row["Timestamp"])
 
- 
 if __name__ == "__main__": 
 	main()
